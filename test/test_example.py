a5d32b29802aded66fdc69d55cab39b7d51ef0c6
Niklas <nip4721@thi.de>
2024-04-16 17:31:51 +0000

    add example.py

--- test/test_example.py
+++ test/test_example.py
@@ -1,12 +1,11 @@
 import pytest
 
+from example import capital_case
 
-def capital_case(x):
-    return x.capitalize()
 
 def test_capital_case():
     assert capital_case('semaphore') == 'Semaphore'
 
 def test_raises_exception_on_non_string_arguments():
     with pytest.raises(AttributeError):
-        capital_case(9)
+        capital_case(9) 
